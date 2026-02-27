4b3296ac57ff9c8a788dac8f04635d1ad0c507e5
jisung-louis <khkh9357@naver.com>
2026-02-27 14:53:18 +0900

    Initial Setting

---
+++ src/test/java/com/team2/hrmanagement/HrmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team2.hrmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
